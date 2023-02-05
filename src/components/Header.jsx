0348fa71d25d7d6b4583c7a47f64e767d1d1dfc5
NunezAN <alex101nunez@gmail.com>
2023-02-05 10:45:09 -0600

    completed header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-teal-700 p-4">
+      <ul className="text-white text-2xl font-extrabold flex items-center justify-between">
+        <li>
+          <a href="">MENU</a>
+        </li>
+        <li>
+          <a href="">EVENT ROOM</a>
+        </li>
+        <li>
+          <a href="">CONTACT</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
